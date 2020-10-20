6348ca483f7ddd75aa971baff33f03030ec51a7b
Arpita-22 <duttaarpita22@gmail.com>
2020-10-20 00:08:46 -0700

    frontend design

---
+++ frontend/src/index.js
@@ -0,0 +1,54 @@
+let snd = new Audio("asset/nmm_moves.mp3");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    createTable(13, 13);
+});
+
+//row, column
+const buttonPositions = {
+                        "0-0": true,
+                        "0-6": true,
+                        "0-12": true,
+                        "6-0": true,
+                        "6-12": true,
+                        "12-0": true,
+                        "12-6": true,
+                        "12-12": true,
+                        "2-2": true,
+                        "2-6": true,
+                        "2-10": true,
+                        "6-2": true,
+                        "6-10": true,
+                        "10-2": true,
+                        "10-6": true,
+                        "10-10": true,
+                        "4-4": true,
+                        "4-6": true,
+                        "4-8": true,
+                        "6-4": true,
+                        "6-8": true,
+                        "8-4": true,
+                        "8-6": true,
+                        "8-8": true,
+                    };
+
+function createTable(rn, cn){
+    for(let r = 0; r< parseInt(rn, 10); r++){
+        let x = document.getElementById('nmm').insertRow(r);
+        for(let c = 0; c < parseInt(cn, 10); c++) {
+            let y =  x.insertCell(c);
+            let id = r.toString().concat("-" +c.toString());
+            if(buttonPositions[id] === true){
+                y.innerHTML=`<button onclick= "handleEvent(id)" id=${id} class='button'/>`
+            } else {
+                //y.innerHTML="R" + r + "C" + c;
+                y.innerHTML="<div class='default-position'/>";
+            }
+        }
+    }
+}
+
+function handleEvent(id){
+    snd.play();
+    alert(id);
+}
